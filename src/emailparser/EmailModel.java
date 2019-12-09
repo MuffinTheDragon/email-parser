47be4fe85b68f30ff79f6fe1c50ca4877fdfd3bb
Dhaval Malhotra <dhaval.malhotra@mail.utoronto.ca>
2019-12-08 22:22:19 -0500

    Add email model This commi adds a module responsible for storing the information parsed by the e-mail parser.

---
+++ src/emailparser/EmailModel.java
@@ -0,0 +1,45 @@
+package emailparser;
+
+import java.util.ArrayList;
+
+/**
+ * This module stores the main information from the parser. 
+ * @author Dhaval
+ *
+ */
+public class EmailModel {
+	
+	private String addressTo;
+	private String addressFrom;
+	private String subject;
+	private String body;
+	
+	public EmailModel() {
+		this.addressTo = "";
+		this.addressFrom = "";
+		this.subject = "";
+		this.body = "";
+	}
+	
+	public void setAddressTo(String addressTo) {
+		this.addressTo = addressTo;
+	}
+	
+	public void setAddressFrom(String addressFrom) {
+		this.setAddressFrom(addressFrom);
+	}
+	
+	public void setSubject(String subject) {
+		this.setSubject(subject);
+	}
+	
+	public void setBody(String body) {
+		this.body += body;
+	}
+	
+	public String[] getInfo() {
+		String[] array = {this.addressTo, this.addressFrom, this.subject, this.body};
+		return array;
+	}
+
+}
